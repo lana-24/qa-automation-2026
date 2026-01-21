4653769066e6a25218afdaac4396137d72964996
olansolve <maulmalikib@gmail.com>
2026-01-21 20:31:55 +0700

    feat(images):[WIP] update code and fix bug

--- the_cat_api/tests/conftest.py
+++ the_cat_api/tests/conftest.py
@@ -1,4 +1,5 @@
 # you can change the logic according the situation
+import requests
 import pytest
 from configs.settings import base_url,token
 
@@ -8,5 +9,23 @@ def headers_token():
              "X-API-Key" : token }
 
 @pytest.fixture
+def headers():
+    return { "Accept" : "application/json" }
+
+
+@pytest.fixture
 def url():
     return base_url
+
+@pytest.fixture
+def get_id(headers_token):
+    files = {'file': ('images.jpeg', open('images.jpeg', 'rb'))}
+    r = requests.post(f'{url}/images/upload',headers=headers_token ,files=files)
+    assert r.status_code == 201
+    img_id = r.json().get('id')
+
+    yield img_id
+    response = requests.get(f'{url}/images/{img_id}/analysis')
+    if response.status_code != 404:
+        rm = requests.delete(f'{url}/image/{img_id}')
+        assert rm.status_code == 200
--- the_cat_api/tests/test_images.py
+++ the_cat_api/tests/test_images.py
@@ -1,15 +1,20 @@
-from utils.helpers import requests_post ,requests_get ,requests_put, reqeusts_patch ,requests_delete
+import requests
+import pytest
+from utils.helpers import method_post, method_get, method_put, method_patch, method_delete
+from typing import Optional,Any
+import jsonschema
 
-def json_schema(schema_name):
+def json_schema(data: dict[str, Any],schema_name: str):
     schemas = {
         "search" : {"type" : "object",
                     "properties" : {"id" : {"type" : "string"},
                                     "url" : {"type" : "string"},
                                     "width" : {"type" : "integer"},
                                     "height" : {"type" : "integer"},
-                                    "mime_type" : {"type" : "string"}}
+                                    "mime_type" : {"type" : "string"}},
                     "required" : ["id","url"]
                     },
+        
         "upload" : {"type" : "object",
                     "properties" : {"id" : {"type" : "string"},
                                     "url" : {"type" : "string"},
@@ -17,28 +22,38 @@ def json_schema(schema_name):
                                     "height" : {"type" : "integer"},
                                     "original_filename" : {"type" : "string"},
                                     "approved" : {"type" : "string"}
-                                   }
+                                    },
                     "required" : ["id","url" ,"original_filename"]
                     },
         "list_upload" : {"type" : "array",
                          "items" : {
                              "type" : "object",
-                             "properties" : {"id" : {"type" : "string"},
-                                             "url" : {"type" : "string"},
-                                             "width" : {"type" : "integer"},
-                                             "height" : {"type" : "integer"},
-                                             "mime_type" : {"type" : "string"},
-                                             "created_at" : {"type" : "string"}}
-                             "required" : ["id","url","created_at"]     
+                             "properties" : {"id" : {"type": "string"},
+                                             "url" : {"type": "string"},
+                                             "width" : {"type": ["integer","null"]},
+                                             "height" : {"type":["integer","null"]},
+                                             "mime_type" : {"type": "string"}
+                                             },
+                             "required" : ["id","url",]}
+                         },
+        "analysis" : {"type" : "array",
+                         "items" : {
+                             "type" : "object",
+                             "properties" : {"image_id" : {"type": "string"},
+                                             "created_at" : {"type": "string"},
+                                             "vendor" : {"type": "string"},
+                                             
+                                             },
+                             "required" : ["image_id","created_at"]     
                          }
-                         }
+                }
         
     }
 
     schema = schemas.get(schema_name)
     jsonschema.validate(data, schema)
     
-def test_images_search(url):
+def test_images_search(headers):
     '''
     TC: P-IMAGES-01
     Search random images without token
@@ -48,8 +63,12 @@ def test_images_search(url):
     - response body type object
     - response body field['id'->string,'url'->string,'mime_type'->string]
     '''
-
-def test_images_search_with_token(url ,headers_token):
+    
+    response = method_get(endpoint='/images/search',headers=headers)
+    assert response.status_code == 200 ,f'not 200 but {response.status_code}'
+    json_schema(response.json(),'search')
+    
+def test_images_search_with_token(headers_token):
     '''
     TC: P-IMAGES-02
     Search random images with token
@@ -59,8 +78,11 @@ def test_images_search_with_token(url ,headers_token):
     - response body type object
     - response body field['id','url','mime_type']
     '''
+    response = method_get(endpoint='/images/search',headers=headers_token)
+    assert response.status_code == 200 ,f'not 200 but {response.status_code}'
+    json_schema(response.json(),'search')
 
-def test_images_upload(url ,headers_token):
+def test_images_upload(url,headers_token):
     '''
     TC: P-IMAGES-03
     Upload image cat
@@ -70,10 +92,16 @@ def test_images_upload(url ,headers_token):
     - response body type object
     - response body field['id','url','original_filename']
     '''
-    files = {'file': ('kucingku.jpg', open('kucingku.jpg', 'rb'))}
-    # Requests akan handle closing-nya
-
-def test_image_myupload(url ,headers_token):
+    files = {'file': ('images.jpeg', open('images.jpeg', 'rb'))}
+    response = method_post(endpoint='/images/upload', headers=headers_token, files=files)
+    assert response.status_code == 201 ,f'not 200 but {response.status_code}'
+    json_schema(response.json(),'upload')
+    # now delete
+    image_id = response.json().get('id')
+    rm =requests.delete(f'{url}/images/{image_id}')
+    assert rm.status_code == 200 ,f'not 200 but {response.status_code}'
+     
+def test_images_myupload(headers_token):
     '''
     TC: P-IMAGES-04
     get my images 
@@ -83,8 +111,11 @@ def test_image_myupload(url ,headers_token):
     - response body type array
     - response body field['id','url','created_at']
     '''
+    response = method_get(endpoint='/images/',headers=headers_token)
+    assert response.status_code == 200 ,f'not 200 but {response.status_code}'
+    json_schema(response.json(),'list_upload')
 
-def test_analysis_myupload(url,headers_token):
+def test_analysis_myupload(headers_token,get_id):
     '''
     TC: P-IMAGES-05
     Analysis my image
@@ -94,19 +125,28 @@ def test_analysis_myupload(url,headers_token):
     - response body type object
     - response body field['image_id','created_at']
     '''
-
-def test_upload_images_breeds(url,headers_token):
+    response = method_get(endpoint= f'/images/{get_id}/analysis', headers=headers_token)
+    assert response.status_code == 200 ,f'not 200 but {response.status_code}'
+    json_schema(response.json(),'analysis')
+    
+def test_upload_images_breeds(headers_token,get_id):
     '''
-    TC: P-IMAGES-06
+    #TC: P-IMAGES-06
     Upload my image breeds
     Endpoint: post /images/{image_id}/breeds
     Expected:
     - status code 200
     - response body type object
     - response body field 'breeds_id'
-    '''
 
-def test_get_images_breeds(url,headers_token):
+    
+    '''
+    response = method_post(endpoint= f'/images/{get_id}/breeds',headers=headers_token)
+    
+    assert response.status_code == 200 ,f'not 200 but {response.status_code}'
+    assert 'breed_id' in response.json()
+
+def test_get_images_breeds(headers_token,get_id):
     '''
     TC: P-IMAGES-07
     Get my image breeds
@@ -116,19 +156,25 @@ def test_get_images_breeds(url,headers_token):
     - response body type object
     - response body field 'breeds_id'
     '''
+    response = method_post(endpoint= f'/images/{get_id}/breeds',headers=headers_token)
+    assert response.status_code == 200 ,f'not 200 but {response.status_code}'
+    assert 'breed_id' in response.json()
 
-def test_delete_images_breeds(url,headers_token):
+def test_delete_images_breeds(headers_token,get_id):
     '''
     TC: P-IMAGES-08
     Delete my image breeds
-    Endpoint: delete /images/{image_id}/breeds
+    Endpoint: delete /images/{image_id}/breeds/{breed_id}
     Expected:
     - status code 200
     - response body type object
     - response body field 'breeds_id'
     '''
+    response = method_get(endpoint= f'/images/{get_id}/breeds',headers=headers_token)
+    assert response.status_code == 200 ,f'not 200 but {response.status_code}'
+    assert 'breed_id' in response.json()
     
-def test_delete_myupload(url ,headers_token):
+def test_delete_myupload(headers_token,get_id):
     '''
     TC: P-IMAGES-09
     Delete my upload
@@ -136,3 +182,5 @@ def test_delete_myupload(url ,headers_token):
     Expected:
     - status code 200
     '''
+    response = method_delete(endpoint= f'/images/{get_id}/breeds', headers=headers_token)
+    assert response.status_code == 200
